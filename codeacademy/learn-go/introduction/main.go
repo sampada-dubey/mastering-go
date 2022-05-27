a86bd92363d38d66ce9ad5c1d78ba278e41c2dd3
Sampada Dubey <freelance.sampada.dubey@gmail.com>
2022-05-28 02:16:16 +0530

    GH-1: Codeacademy - Learn Go

---
+++ codeacademy/learn-go/introduction/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	t "time"
+
+	m "math"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("Now - ", t.Now())
+	fmt.Println("Random value - ", m.Pi)
+}
